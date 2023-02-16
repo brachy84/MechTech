81cc4e07364979108784e362a286793e38a5e6d9
brachy84 <brachy84@gmail.com>
2023-02-16 17:34:53 +0100

    1.2.0

--- src/main/java/com/brachy84/mechtech/MechTech.java
+++ src/main/java/com/brachy84/mechtech/MechTech.java
@@ -22,9 +22,10 @@ import org.apache.logging.log4j.Logger;
 
 @Mod(modid = MechTech.MODID, name = MechTech.NAME, version = MechTech.VERSION, dependencies = "required:forge@[14.23.5.2847,);" + GTValues.MOD_VERSION_DEP + "after:crafttweaker")
 public class MechTech {
+
     public static final String MODID = "mechtech";
     public static final String NAME = "MechTech";
-    public static final String VERSION = "1.1.0-beta";
+    public static final String VERSION = "1.2.0";
 
     public static final Logger logger = LogManager.getLogger("MechTech");
     @SidedProxy(modId = MODID, clientSide = "com.brachy84.mechtech.common.ClientProxy", serverSide = "com.brachy84.mechtech.common.CommonProxy")
